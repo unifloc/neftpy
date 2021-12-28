0345d84e272e70222d32150d98d0838fe82630e3
khabibullinra <khabibullinra@gmail.com>
2021-12-28 18:16:56 +0300

    updated bug with uconverter

--- neftpy/upvt.py
+++ neftpy/upvt.py
@@ -36,7 +36,8 @@ def unf_pb_Standing_MPaa(rsb_m3m3, gamma_oil=0.86, gamma_gas=0.6, t_K=350):
     # for large rsb values do not correct what the correlation gives
     return np.where(rsb_old < min_rsb, 
                                       (pb_MPaa - 0.1013) * rsb_old / min_rsb + 0.1013, 
-                                      pb_MPaa)
+                                      pb_MPaa
+                    )
 
 
 def unf_pb_Valko_MPaa(rsb_m3m3, gamma_oil=0.86, gamma_gas=0.6, t_K=350):
@@ -62,33 +63,20 @@ def unf_pb_Valko_MPaa(rsb_m3m3, gamma_oil=0.86, gamma_gas=0.6, t_K=350):
     z1 = -4.81413889469569 + 0.748104504934282 * np.log(rsb_m3m3) \
         + 0.174372295950536 * np.log(rsb_m3m3) ** 2 - 0.0206 * np.log(rsb_m3m3) ** 3
 
-    #init
-    #z2 = 25.537681965 - 57.519938195 / gamma_oil + 46.327882495 / gamma_oil ** 2 - 13.485786265 / gamma_oil ** 3 init
-    #vba
-    api = uc.gamma_oil2api(gamma_oil)
+    api = uc.gamma_oil_2_api(gamma_oil)
     z2 = 1.27 - 0.0449 * api + 4.36 * 10 ** (-4) * api ** 2 - 4.76 * 10 ** (-6) * api ** 3
     z3 = 4.51 - 10.84 * gamma_gas + 8.39 * gamma_gas ** 2 - 2.34 * gamma_gas ** 3
-
-    #init
-    #z4 = 6.00696e-8 * t_K ** 3 - 8.554832172e-5 * t_K ** 2 + 0.043155018225018 * t_K - 7.22546617091445
-    # vba
     z4 = -7.2254661 + 0.043155 * t_K - 8.5548 * 10 ** (-5) * t_K ** 2 + 6.00696 * 10 ** (
         -8) * t_K ** 3
-
     z = z1 + z2 + z3 + z4
-
-    #init
-    #pb_atma = 119.992765886175 * np.exp(0.0075 * z ** 2 + 0.713 * z)
-    #pb_MPaa = pb_atma / 10.1325
-    #vba
     lnpb = 2.498006 + 0.713 * z + 0.0075 * z ** 2
     pb_MPaa = 2.718282 ** lnpb
     """
-    для низких значений газосодержания зададим асимптотику Pb = 1 атма при Rsb = 0
-    корреляция Valko получена с использованием непараметрической регресии GRACE метод (SPE 35412)
-    особенность подхода - за пределеми интервала адаптации ассимптотики не соблюдаются
-    поэтому их устанавливаем вручную
-    для больших значений газосодержания продолжим линейный тренд корреляции
+    for low values of gas content we set the asymptotics Pb = 1 atm with Rsb = 0
+    the Valko correlation is obtained using the GRACE nonparametric regression method (SPE 35412)
+    The peculiarity of this approach is that beyond the adaptation interval the asymptotics are not observed
+    therefore they are set manually
+    for large values of gas content we continue the linear trend of correlation
     """
 
     pb_MPaa = np.where(rsb_old < min_rsb, 
