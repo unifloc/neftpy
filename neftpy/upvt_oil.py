a016ab7581db096910b722d9c585184d5b65ffea
Khabibullin Rinat <khabibullinra@gmail.com>
2024-12-16 13:12:20 +0300

    explicit const and units converter imports

--- neftpy/upvt_oil.py
+++ neftpy/upvt_oil.py
@@ -1,5 +1,5 @@
-from neftpy.uconvert import *
-from neftpy.uconst import *
+import neftpy.uconvert as uc
+import neftpy.uconst as uconst
 
 import numpy as np
 import scipy.optimize as opt
@@ -40,11 +40,11 @@ def unf_pb_Standing_MPaa(t_K:FloatArray=350,
     rsb_old = np.copy(rsb_m3m3)
     mask = rsb_old < min_rsb
     rsb_m3m3[mask] = min_rsb
-    yg = RHO_AIR_kgm3 + 0.001648 * t_K - 1.769 / gamma_oil         # gas molar fraction
+    yg = uconst.RHO_AIR_kgm3 + 0.001648 * t_K - 1.769 / gamma_oil         # gas molar fraction
     pb_MPaa = np.array(0.5197 * (rsb_m3m3 / gamma_gas) ** 0.83 * 10 ** yg)
     # for low rsb values, we set the asymptotics Pb = 1 atma at Rsb = 0
     # for large rsb values do not correct what the correlation gives
-    pb_MPaa[mask] = (pb_MPaa[mask] - P_SC_MPa) * rsb_old[mask] / min_rsb + P_SC_MPa
+    pb_MPaa[mask] = (pb_MPaa[mask] - uconst.P_SC_MPa) * rsb_old[mask] / min_rsb + uconst.P_SC_MPa
     return pb_MPaa
 
 
@@ -93,8 +93,8 @@ def unf_pb_Valko_MPaa(t_K:FloatArray=350,
     поэтому их устанавливаем вручную
     для больших значений газосодержания продолжим линейный тренд корреляции
     """
-    pb_MPaa[mask_min] = (pb_MPaa[mask_min] - P_SC_MPa) * rsb_old[mask_min] / min_rsb + P_SC_MPa
-    pb_MPaa[mask_max] = (pb_MPaa[mask_max] - P_SC_MPa) * rsb_old[mask_max] / max_rsb + P_SC_MPa
+    pb_MPaa[mask_min] = (pb_MPaa[mask_min] - uconst.P_SC_MPa) * rsb_old[mask_min] / min_rsb + uconst.P_SC_MPa
+    pb_MPaa[mask_max] = (pb_MPaa[mask_max] - uconst.P_SC_MPa) * rsb_old[mask_max] / max_rsb + uconst.P_SC_MPa
     return pb_MPaa
 
 
@@ -116,12 +116,12 @@ def unf_pb_Glaso_MPaa(t_K:FloatArray=350,
     """
 
     #  можно дополнить код, поправками на неуглеводородные составляющие в нефти, в статье есть
-    api = gamma_oil_2_api(gamma_oil)
-    t_F = K_2_F(t_K)
-    rs_scfstb = m3m3_2_scfstb(rsb_m3m3)
+    api = uc.gamma_oil_2_api(gamma_oil)
+    t_F = uc.K_2_F(t_K)
+    rs_scfstb = uc.m3m3_2_scfstb(rsb_m3m3)
     pb = (rs_scfstb / gamma_gas) ** 0.816 * (t_F ** 0.172 / api ** 0.989)
     log_pb = np.log10(pb)
-    pb = psi_2_MPa(10 ** (1.7669 + 1.7447 * log_pb - 0.30218 * log_pb ** 2)) 
+    pb = uc.psi_2_MPa(10 ** (1.7669 + 1.7447 * log_pb - 0.30218 * log_pb ** 2)) 
     return pb
 
 
@@ -160,7 +160,7 @@ def unf_rs_Standing_m3m3(p_MPaa:FloatArray=1,
     может считать в случае если нет давления насыщения и газосодержания при давлении насыщения, корреляция не точная
     """
 
-    yg = RHO_AIR_kgm3 + 0.001648 * t_K - 1.769 / gamma_oil
+    yg = uconst.RHO_AIR_kgm3 + 0.001648 * t_K - 1.769 / gamma_oil
 
     rs_m3m3 = np.where(pb_MPaa * rsb_m3m3 == 0, 
                         gamma_gas * (1.92 * p_MPaa / 10 ** yg) ** 1.204, 
@@ -198,7 +198,7 @@ def unf_drs_dp_Standing_m3m3(p_MPaa:FloatArray=1,
     может считать в случае если нет давления насыщения и газосодержания при давлении насыщения, корреляция не точная
     """
 
-    yg = RHO_AIR_kgm3 + 0.001648 * t_K - 1.769 / gamma_oil
+    yg = uconst.RHO_AIR_kgm3 + 0.001648 * t_K - 1.769 / gamma_oil
 
     drs_dp = np.where(pb_MPaa * rsb_m3m3 == 0, 
                         gamma_gas * (1.92 / 10**yg) ** 1.204 * 1.204 * p_MPaa**0.204, 
@@ -228,12 +228,12 @@ def unf_rsb_Standing_m3m3(pb_atma:FloatArray=10,
 
     """
 
-    return unf_rs_Standing_m3m3(p_MPaa=psp_MPaa, 
-                                t_K=tsp_K,
-                                pb_MPaa=0,
-                                rsb_m3m3=rsp_m3m3, 
-                                gamma_oil=gamma_oil, 
-                                gamma_gas=0.8)
+    #return unf_rs_Standing_m3m3(p_MPaa=psp_MPaa, 
+    #                            t_K=tsp_K,
+    #                            pb_MPaa=0,
+    #                            rsb_m3m3=rsp_m3m3, 
+    #                            gamma_oil=gamma_oil, 
+    #                            gamma_gas=0.8)
 
 def _unf_rs_Velarde_m3m3_(p_MPaa:float=1, 
                           t_K:float=350,
@@ -261,19 +261,19 @@ def _unf_rs_Velarde_m3m3_(p_MPaa:float=1,
     # на всякий случай тут старая не векторизованная реализация
 
         
-    pb_estimation_Valko_McCain = unf_pb_Valko_MPaa(RS_MAX_Velarde, gamma_oil, gamma_gas, t_K)
+    pb_estimation_Valko_McCain = unf_pb_Valko_MPaa(uconst.RS_MAX_Velarde, gamma_oil, gamma_gas, t_K)
     if (pb_MPaa > pb_estimation_Valko_McCain):
         if p_MPaa < pb_MPaa:
             return rsb_m3m3 * p_MPaa / pb_MPaa
         else:
             return rsb_m3m3
         
-    api = gamma_oil_2_api(gamma_oil)
-    t_F = K_2_F(t_K)
-    pb_psig = MPa_2_psi(pb_MPaa) - P_SC_PSI
+    api = uc.gamma_oil_2_api(gamma_oil)
+    t_F = uc.K_2_F(t_K)
+    pb_psig = uc.MPa_2_psi(pb_MPaa) - uconst.P_SC_PSI
 
     if pb_psig > 0:
-        pr = MPa_2_psig(p_MPaa)  / pb_psig
+        pr = uc.MPa_2_psig(p_MPaa)  / pb_psig
     else:
         pr = 0
 
@@ -287,7 +287,7 @@ def _unf_rs_Velarde_m3m3_(p_MPaa:float=1,
         a1 = A[0] * gamma_gas ** A[1] * api ** A[2] * t_F ** A[3] * pb_psig ** A[4]
         a2 = B[0] * gamma_gas ** B[1] * api ** B[2] * t_F ** B[3] * pb_psig ** B[4]
         a3 = C[0] * gamma_gas ** C[1] * api ** C[2] * t_F ** C[3] * pb_psig ** C[4]
-        pr = MPa_2_psig(p_MPaa)  / pb_psig
+        pr = uc.MPa_2_psig(p_MPaa)  / pb_psig
         rsr = a1 * pr ** a2 + (1 - a1) * pr ** a3
         rs_m3m3 = rsr * rsb_m3m3
     else:
@@ -320,21 +320,21 @@ def unf_rs_Velarde_m3m3(p_MPaa:FloatArray=1,
 
     """
     pb_MPaa = np.array(pb_MPaa)
-    pr = np.where(pb_MPaa > P_SC_MPa, 
-                  MPa_2_psig(p_MPaa)/(MPa_2_psig(pb_MPaa)), 
+    pr = np.where(pb_MPaa > uconst.P_SC_MPa, 
+                  uc.MPa_2_psig(p_MPaa)/(uc.MPa_2_psig(pb_MPaa)), 
                   0
                   )
 
     _go_ = -0.929328621908127 + 1 / gamma_oil
     _t_ = 0.0039158526769204 * t_K  - 1
-    _pb_ = pb_MPaa - P_SC_MPa
+    _pb_ = pb_MPaa - uconst.P_SC_MPa
 
     a1 = (0.0849029848623362 * gamma_gas**1.672608 * _go_**0.92987 *_t_**0.247235 * _pb_**1.056052) 
     a2 = (1.20743882814017 * _go_ ** 0.337711 * _t_** 0.132795 * _pb_**0.302065) / (gamma_gas ** 1.00475)
     a3 = 0.231607087371213 * _pb_ ** 0.047094 / ( gamma_gas**1.48548 * _go_**0.164741 * _t_**0.09133)
 
     
-    pb_estimation_Valko_McCain = unf_pb_Valko_MPaa(rsb_m3m3=RS_MAX_Velarde,
+    pb_estimation_Valko_McCain = unf_pb_Valko_MPaa(rsb_m3m3=uconst.RS_MAX_Velarde,
                                                    gamma_oil=gamma_oil,
                                                    gamma_gas=gamma_gas,
                                                    t_K=t_K)
@@ -377,10 +377,10 @@ def unf_rsb_Mccain_m3m3(rsp_m3m3:FloatArray=10,
     J. VELARDE, W.D. MCCAIN, 2002
     """
 
-    rsp_scfstb = m3m3_2_scfstb(rsp_m3m3)
-    api = gamma_oil_2_api(gamma_oil)
-    psp_psia = MPa_2_psi(psp_MPaa)
-    tsp_F = K_2_F(tsp_K)
+    rsp_scfstb = uc.m3m3_2_scfstb(rsp_m3m3)
+    api = uc.gamma_oil_2_api(gamma_oil)
+    psp_psia = uc.MPa_2_psi(psp_MPaa)
+    tsp_F = uc.K_2_F(tsp_K)
 
     nplogpsp_psia = np.log(psp_psia)
     nplogapi = np.log(api)
@@ -398,7 +398,7 @@ def unf_rsb_Mccain_m3m3(rsp_m3m3:FloatArray=10,
                             )
                    )
 
-    return scfstb_2_m3m3(rsb)
+    return uc.scfstb_2_m3m3(rsb)
 
 """ 
 ====================================================================================================
@@ -452,7 +452,7 @@ def unf_bo_below_pb_m3m3(rho_oil_st_kgm3:FloatArray=820,
     ref1 book Mccain_w_d_spivey_j_p_lenn_c_p_petroleum_reservoir_fluid,third edition, 2011
     """
     # коэффициенты преобразованы - смотри описанию в ноутбуке
-    bo = (rho_oil_st_kgm3 + RHO_AIR_kgm3 * rs_m3m3 * gamma_gas) / rho_oil_insitu_kgm3
+    bo = (rho_oil_st_kgm3 + uconst.RHO_AIR_kgm3 * rs_m3m3 * gamma_gas) / rho_oil_insitu_kgm3
     return bo
 
 
@@ -473,8 +473,8 @@ def unf_bo_saturated_Standing_m3m3(t_K:FloatArray=300,
     ref1 Volumetric and phase behavior of oil field hydrocarbon systems / M.B. Standing Standing, M. B. 1981
     """
 
-    rs_scfstb = m3m3_2_scfstb(rs_m3m3)
-    t_F = K_2_F(t_K)
+    rs_scfstb = uc.m3m3_2_scfstb(rs_m3m3)
+    t_F = uc.K_2_F(t_K)
     return 0.972 + 1.47e-4 * (rs_scfstb * (gamma_gas / gamma_oil) ** 0.5 + 1.25 * t_F) ** 1.175
 
 
@@ -495,8 +495,8 @@ def unf_bo_saturated_Glaso_m3m3(t_K:FloatArray,
     ref Generalized Pressure-Volume-Temperature Correlations, Glaso, 1980
     """
 
-    t_F = K_2_F(t_K)
-    rs_scfstb = m3m3_2_scfstb(rs_m3m3)
+    t_F = uc.K_2_F(t_K)
+    rs_scfstb = uc.m3m3_2_scfstb(rs_m3m3)
     bob = rs_scfstb * (gamma_gas / gamma_oil) ** 0.526 + 0.968 * t_F
     bob = 10 ** (-6.58511 + 2.91329 * np.log10(bob) - 0.27683 * np.log10(bob) ** 2) + 1
     return bob
@@ -521,9 +521,9 @@ def unf_bo_below_Glaso_m3m3(p_MPaa:FloatArray,
     ref Generalized Pressure-Volume-Temperature Correlations, Glaso, 1980
     """
 
-    t_F = K_2_F(t_K)
-    rs_scfstb = m3m3_2_scfstb(rs_m3m3)
-    p_psia = MPa_2_psi(p_MPaa)
+    t_F = uc.K_2_F(t_K)
+    rs_scfstb = uc.m3m3_2_scfstb(rs_m3m3)
+    p_psia = uc.MPa_2_psi(p_MPaa)
     log_bt = np.log10( rs_scfstb * t_F ** 0.5 / gamma_gas ** 0.3 * gamma_oil ** (2.9 * 10 ** (-0.00027 * rs_scfstb)) * p_psia ** -1.1089)
 
     return 10 ** (8.0135e-2 + 4.7257e-1 * log_bt + 1.7351e-1 * log_bt ** 2)
@@ -560,9 +560,9 @@ def unf_rho_oil_Mccain_kgm3(p_MPaa:FloatArray=1,
 
     ref1 book Mccain_w_d_spivey_j_p_lenn_c_p_petroleum_reservoir_fluid,third edition, 2011
     """
-    rs_scfstb = m3m3_2_scfstb(rs_m3m3)
-    t_F = K_2_F(t_K)
-    p_psia = MPa_2_psi(p_MPaa)
+    rs_scfstb = uc.m3m3_2_scfstb(rs_m3m3)
+    t_F = uc.K_2_F(t_K)
+    p_psia = uc.MPa_2_psi(p_MPaa)
 
     gamma_gassp = np.where(gamma_gassp == 0, gamma_gas, gamma_gassp)
 
@@ -577,7 +577,7 @@ def unf_rho_oil_Mccain_kgm3(p_MPaa:FloatArray=1,
     gamma_gassp, gamma_gas, gamma_oil, rs_scfstb = np.broadcast_arrays(gamma_gassp, gamma_gas, gamma_oil, rs_scfstb)
     ro_po = 52.8 - 0.01 * rs_scfstb  # первое приближение
     ro_po = opt.fsolve(ro_po_equation, ro_po, (gamma_gassp, gamma_gas, gamma_oil, rs_scfstb))
-    pb_psia = MPa_2_psi(pb_MPaa)
+    pb_psia = uc.MPa_2_psi(pb_MPaa)
     p = np.where(p_MPaa < pb_MPaa, p_psia, pb_psia)
 
     dro_p = ((0.167 + 16.181 * 10 ** (-0.0425 * ro_po)) * (p / 1000) - 0.01 * 
@@ -588,9 +588,9 @@ def unf_rho_oil_Mccain_kgm3(p_MPaa:FloatArray=1,
     ro_or = ro_bs - dro_t
 
     ro_or = np.where(p_MPaa >= pb_MPaa, 
-                     ro_or * np.exp(compr_1pa_2_1psi(co_1MPa / 1e6) * (p_psia - pb_psia)),
+                     ro_or * np.exp(uc.compr_1pa_2_1psi(co_1MPa / 1e6) * (p_psia - pb_psia)),
                      ro_or)  
-    return  lbft3_2_kgm3(ro_or)
+    return  uc.lbft3_2_kgm3(ro_or)
 
 
 def unf_rho_oil_Standing_kgm3(p_MPaa:FloatArray=1, 
@@ -615,7 +615,7 @@ def unf_rho_oil_Standing_kgm3(p_MPaa:FloatArray=1,
 
     ref1 book Brill 2006, Production Optimization Using Nodal Analysis
     """
-    po = (RHO_WATER_SC_kgm3 * gamma_oil + RHO_AIR_kgm3 * gamma_gas * rs_m3m3) / bo_m3m3
+    po = (uconst.RHO_WATER_SC_kgm3 * gamma_oil + uconst.RHO_AIR_kgm3 * gamma_gas * rs_m3m3) / bo_m3m3
         
     return np.where(p_MPaa > pb_MPaa, po * np.exp(co_1MPa * (p_MPaa - pb_MPaa)), po)
 
@@ -646,14 +646,14 @@ def unf_compressibility_saturated_oil_McCain_1Mpa(p_MPa:FloatArray=1,
 
     ref2 https://www.onepetro.org/download/journal-paper/SPE-15664-PA?id=journal-paper%2FSPE-15664-PA
     """
-    rsb_scfstb = m3m3_2_scfstb(rsb_m3m3)
-    t_F = K_2_F(t_K)
-    api = gamma_oil_2_api(gamma_oil)
-    p_psia = MPa_2_psi(p_MPa)
-    pb_psia = MPa_2_psi(pb_MPa)
+    rsb_scfstb = uc.m3m3_2_scfstb(rsb_m3m3)
+    t_F = uc.K_2_F(t_K)
+    api = uc.gamma_oil_2_api(gamma_oil)
+    p_psia = uc.MPa_2_psi(p_MPa)
+    pb_psia = uc.MPa_2_psi(pb_MPa)
     co_1psi = np.exp(-7.573 - 1.450 * np.log(p_psia) - 0.383 * np.log(pb_psia) + 1.402 * np.log(t_F) +
                      0.256 * np.log(api) + 0.449 * np.log(rsb_scfstb))
-    co_1MPa = compr_1psi_2_1MPa(co_1psi)
+    co_1MPa = uc.compr_1psi_2_1MPa(co_1psi)
     return co_1MPa
     #TODO в тесте большое расхожнение, надо бы сравнить с vba и разобраться
 
@@ -679,13 +679,13 @@ def unf_compressibility_oil_VB_1Mpa(p_MPaa:FloatArray,
 
     """
 
-    rs_scfstb = m3m3_2_scfstb(rs_m3m3)
-    t_F = K_2_F(t_K)
-    api = gamma_oil_2_api(gamma_oil)
-    p_psia = MPa_2_psi(p_MPaa)
+    rs_scfstb = uc.m3m3_2_scfstb(rs_m3m3)
+    t_F = uc.K_2_F(t_K)
+    api = uc.gamma_oil_2_api(gamma_oil)
+    p_psia = uc.MPa_2_psi(p_MPaa)
     
     return np.where(p_MPaa > 0, 
-                    compr_1psi_2_1MPa( (-1433 + 5 * rs_scfstb + 17.2 * t_F - 1180 * gamma_gas + 12.61 * api) / (1e5 * p_psia)),
+                    uc.compr_1psi_2_1MPa( (-1433 + 5 * rs_scfstb + 17.2 * t_F - 1180 * gamma_gas + 12.61 * api) / (1e5 * p_psia)),
                     0.0
                     )
 
@@ -696,7 +696,7 @@ def _unf_compessibility_oil_VB_1MPa_vba_(t_K:FloatArray,
                                        gamma_gas:FloatArray,
                                        )->FloatArray:
     co_1atm = (28.1 * rsb_m3m3 + 30.6 * t_K - 1180 * gamma_gas + 1784 / gamma_oil - 10910)
-    return 1/atm_2_bar(1/co_1atm)/10  #*))) **когда не дружишь с математикой ***когда слишком много времени потратил на uniflocpy
+    return 1/uc.atm_2_bar(1/co_1atm)/10  #*))) **когда не дружишь с математикой ***когда слишком много времени потратил на uniflocpy
     #TODO на сравнить с реализацией не vba и понять, что оставить
 
 
@@ -730,11 +730,11 @@ def unf_gamma_gas_Mccain(psp_MPaa:FloatArray,
 
     """
 
-    api = gamma_oil_2_api(gamma_oil)
-    psp_psia = MPa_2_psi(psp_MPaa)
-    tsp_F = K_2_F(tsp_K)
-    rsp_scfstb = m3m3_2_scfstb(rsp_m3m3)
-    rst_scfstb = m3m3_2_scfstb(rst_m3m3)
+    api = uc.gamma_oil_2_api(gamma_oil)
+    psp_psia = uc.MPa_2_psi(psp_MPaa)
+    tsp_F = uc.K_2_F(tsp_K)
+    rsp_scfstb = uc.m3m3_2_scfstb(rsp_m3m3)
+    rst_scfstb = uc.m3m3_2_scfstb(rst_m3m3)
 
     nplogpsp_psia = np.log(psp_psia) 
     nplogrsp_scfstb = np.log(rsp_scfstb)
@@ -779,10 +779,10 @@ def unf_McCain_specificgravity(p_MPaa:FloatArray,
     ref1 book Mccain_w_d_spivey_j_p_lenn_c_p_petroleum_reservoir_fluid,third edition, 2011
     """
 
-    api = gamma_oil_2_api(gamma_oil)
-    rsb_scfstb = m3m3_2_scfstb(rsb_m3m3)
-    p_psia = MPa_2_psi(p_MPaa)
-    t_F = K_2_F(t_K)
+    api = uc.gamma_oil_2_api(gamma_oil)
+    rsb_scfstb = uc.m3m3_2_scfstb(rsb_m3m3)
+    p_psia = uc.MPa_2_psi(p_MPaa)
+    t_F = uc.K_2_F(t_K)
     gamma_gasr = 1 / (-208.0797 / p_psia + 22.885 / p_psia ** 2 - 0.000063641 * p_psia + 3.38346 / t_F ** 0.5 -
                       0.000992 * t_F - 0.000081147 * rsb_scfstb - 0.001956 * api + 1.081956 / gamma_gassp + 0.394035 *
                       gamma_gassp ** 2)
@@ -809,8 +809,8 @@ def unf_viscosity_deadoil_Beggs_cP(t_K:FloatArray,
     Journal of Petroleum Technology. Vol. 27, No. 9 (1975)
 
     """
-    api = gamma_oil_2_api(gamma_oil)
-    t_F = K_2_F(t_K)
+    api = uc.gamma_oil_2_api(gamma_oil)
+    t_F = uc.K_2_F(t_K)
     c = 10 ** (3.0324 - 0.02023 * api) * t_F ** -1.163  
     return 10 ** c - 1
 
@@ -830,7 +830,7 @@ def unf_viscosity_deadoil_BeggsRobinson_cP(t_K:FloatArray,
         https://petrowiki.spe.org/File:Vol1_Page_323_Image_0001.png
     """
     # из vba  unf_pvt_viscosity_dead_oil_Beggs_Robinson_cP
-    x = K_2_F(t_K) ** (-1.163) * np.exp(13.108 - 6.591 / gamma_oil)
+    x = uc.K_2_F(t_K) ** (-1.163) * np.exp(13.108 - 6.591 / gamma_oil)
     return 10 ** x - 1
 
 
@@ -858,8 +858,8 @@ def unf_viscosity_deadoil_Standing(t_K:FloatArray,
 
     # из vba  unf_pvt_viscosity_dead_oil_Standing_cP
 
-    api = gamma_oil_2_api(gamma_oil)
-    return (0.32 + 1.8e7 / api ** 4.53) * (360 / (K_2_F(t_K) + 200)) ** (10 ** (0.43 + 8.33 / api))
+    api = uc.gamma_oil_2_api(gamma_oil)
+    return (0.32 + 1.8e7 / api ** 4.53) * (360 / (uc.K_2_F(t_K) + 200)) ** (10 ** (0.43 + 8.33 / api))
 
 
 def unf_viscosity_oil_Standing_cP(p_MPa:FloatArray,  
@@ -893,7 +893,7 @@ def unf_viscosity_saturatedoil_Beggs_cP(mu_oil_dead_cP:FloatArray,
     Journal of Petroleum Technology. Vol. 27, No. 9 (1975)
 
     """
-    rs_scfstb = m3m3_2_scfstb(rs_m3m3)
+    rs_scfstb = uc.m3m3_2_scfstb(rs_m3m3)
     a = 10.715 * (rs_scfstb + 100) ** -0.515
     b = 5.44 * (rs_scfstb + 150) ** -0.338
     return a * mu_oil_dead_cP ** b
@@ -915,8 +915,8 @@ def unf_viscosity_undersaturatedoil_VB_cP(p_MPaa:FloatArray,
     J Pet Technol 32 (6): 968-970. SPE-6719-PA
 
     """
-    p_psia = MPa_2_psi(p_MPaa)
-    pb_psia = MPa_2_psi(pb_MPaa)
+    p_psia = uc.MPa_2_psi(p_MPaa)
+    pb_psia = uc.MPa_2_psi(pb_MPaa)
     m = 2.6 * p_psia ** 1.187 * np.exp(-11.513 - 8.98e-5 * p_psia)
     viscosity_cP = mu_oilb_cP * (p_psia / pb_psia) ** m
     return viscosity_cP
@@ -962,7 +962,7 @@ def unf_viscosity_undersaturatedoil_Petrosky_cP(p_MPaa:FloatArray,
     """
     log_mu_oilb_cP = np.log(mu_oilb_cP)
     A = -1.0146 + 1.3322 * log_mu_oilb_cP - 0.4876 * log_mu_oilb_cP ** 2 - 1.15036 * log_mu_oilb_cP ** 3
-    return  mu_oilb_cP + 1.3449e-3 * MPa_2_psi(p_MPaa-pb_MPaa) * 10 ** A
+    return  mu_oilb_cP + 1.3449e-3 * uc.MPa_2_psi(p_MPaa-pb_MPaa) * 10 ** A
 
 
 """ 
@@ -985,9 +985,9 @@ def unf_heat_capacity_oil_Gambill_JkgC(gamma_oil:FloatArray,
     Society of Petroleum Engineers, 1999. – Т. 17. in Page 122
     """
 
-    t_F = C_2_F(t_C)
+    t_F = uc.C_2_F(t_C)
     heat_capacity_oil_btulbmF = ((0.388 + 0.00045 * t_F) / gamma_oil ** (0.5))
-    return btulbmF_2_kJkgK(heat_capacity_oil_btulbmF) * 1000
+    return uc.btulbmF_2_kJkgK(heat_capacity_oil_btulbmF) * 1000
 
 
 def unf_heat_capacity_oil_Wes_Wright_JkgC(gamma_oil:FloatArray, 
@@ -1020,7 +1020,7 @@ def unf_thermal_conductivity_oil_Abdul_Seoud_Moharam_WmK(gamma_oil:FloatArray,
     Oil & Gas Science and Technology–Revue d’IFP Energies nouvelles. – 2012. – Т. 67. – №. 3. – С. 451-477.
     """
  
-    return (2.540312 * (gamma_oil / C_2_K(t_C)) ** 0.5) - 0.014485
+    return (2.540312 * (gamma_oil / uc.C_2_K(t_C)) ** 0.5) - 0.014485
 
 
 def unf_thermal_conductivity_oil_Smith_WmK(gamma_oil:FloatArray, 
@@ -1036,7 +1036,7 @@ def unf_thermal_conductivity_oil_Smith_WmK(gamma_oil:FloatArray,
     ref1 Das D. K., Nerella S., Kulkarni D. Thermal properties of petroleum and gas-to-liquid products //
     Petroleum science and technology. – 2007. – Т. 25. – №. 4. – С. 415-425.   """
 
-    return (0.137 / (gamma_oil * 1000) * (1 - 0.00054 * (C_2_K(t_C) - 273)) * 1e3)
+    return (0.137 / (gamma_oil * 1000) * (1 - 0.00054 * (uc.C_2_K(t_C) - 273)) * 1e3)
 
 
 def unf_thermal_conductivity_oil_Cragoe_WmK(gamma_oil:FloatArray, 
@@ -1051,7 +1051,7 @@ def unf_thermal_conductivity_oil_Cragoe_WmK(gamma_oil:FloatArray,
 
     ref1 Das D. K., Nerella S., Kulkarni D. Thermal properties of petroleum and gas-to-liquid products //
     Petroleum science and technology. – 2007. – Т. 25. – №. 4. – С. 415-425.   """
-    t_K = C_2_K(t_C)
+    t_K = uc.C_2_K(t_C)
     return (0.118 /(gamma_oil * 1000) * (1 - 0.00054 * (t_K - 273)) * 10 ** 3)
 
 
